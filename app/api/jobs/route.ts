b0f0d5f4ded0c19233385990858fdef729d5ab78
Ramiro Batallas <rbatallas@oshyn.com>
2025-12-09 21:05:39 -0500

    remove old code

--- app/api/jobs/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-import jobsData from "@/lib/mocks/jobs.json";
-
-export async function GET() {
-  return NextResponse.json(jobsData);
-}
--- app/api/testimonials/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-import testimonialsData from "@/lib/mocks/testimonials.json";
-
-export async function GET() {
-  return NextResponse.json(testimonialsData);
-}
