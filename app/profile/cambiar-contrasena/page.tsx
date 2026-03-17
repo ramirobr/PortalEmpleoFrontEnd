7047b811d27be6b5bd7da978395eb2c18ad4e5e1
Ramiro Batallas <rbatallas@oshyn.com>
2026-03-16 21:18:11 -0500

    remove old project

--- app/profile/cambiar-contrasena/page.tsx
+++
@@ -1,9 +0,0 @@
-import CambiarContrasena from "../components/CambiarContrasena";
-
-export default function CambiarContrasenaPage() {
-  return (
-    <div className="flex-1 flex flex-col items-center justify-center px-4 py-12">
-      <CambiarContrasena />
-    </div>
-  );
-}
--- types/auth.ts
+++
@@ -1,7 +0,0 @@
-export const ROLES = {
-  Postulante: "Postulante",
-  AdministradorEmpresa: "Administrador Empresa",
-  AdministradorSistema: "Administrador Sistema",
-} as const;
-
-export type Roles = typeof ROLES[keyof typeof ROLES];
